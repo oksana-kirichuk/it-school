7a5569733b2aeadbdb704cbc011ea9bc387b6416
Oksana <oksana.kyrychuk1991@gmail.com>
2020-02-09 20:11:35 +0200

    Added slick reset styles

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Slider slick.js
+
+$('.reviews-slider').slick({
+    dots: true,
+    arrows: true,
+
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
