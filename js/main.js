e65661329a23d5924b091ac534106dfbcd096ca5
Oksana <oksana.kyrychuk1991@gmail.com>
2020-02-11 20:00:19 +0200

    Ended a footer

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// Slider slick.js
+// // Slider slick.js
 
-$('.reviews-slider').slick({
-    dots: true,
-    arrows: true,
+// $('.reviews-slider').slick({
+//     dots: true,
+//     arrows: true,
 
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 900,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }
-    ]
-});
+//     responsive: [
+//         {
+//             breakpoint: 1920,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 900,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             }
+//         }
+//     ]
+// });
