d415d4f9af9860b12bdcff5fc7ac53cf25e6ec40
Oksana <oksana.kyrychuk1991@gmail.com>
2020-03-11 14:21:16 +0200

    added the form section

--- js/main.js
+++ js/main.js
@@ -31,3 +31,30 @@
 //     ]
 // });
 
+// $('.set > a').click(function(e) {
+//     let selector = this.getAttribute('data-accordeon');
+  
+//     if($(selector).hasClass('active')) {
+//       $(selector).removeClass('active');
+//       $(this).find('i').removeClass('fa-minus').addClass('fa-plus');
+//     } else {
+//       $('.content').removeClass('active');
+//       $('.set > a > i').removeClass('fa-minus').addClass('fa-plus')
+//       $(selector).addClass('active');
+//       $(this).find('i').removeClass('fa-plus').addClass('fa-minus');
+//     }
+//   });
+
+$('.accordeon-set' > '.accordeon-set-name' > a).click(function(e) {
+    let selector = this.getAttribute('data-accordeon');
+
+    if($(selector).hasClass('active')) {
+        $(selector).removeClass('active')
+    }
+})
+      
+  
+      
+  
+  
+
