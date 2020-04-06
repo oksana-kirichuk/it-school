ce77aef75421d5e96a33eded132ddfa0361dd01f
Oksana <oksana.kyrychuk1991@gmail.com>
2020-04-07 02:01:52 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,79 +1,77 @@
-
+// form inputs
   
-  
-  // form
-  
-  (function() {
-      const inputs = document.querySelectorAll('.form-input'),
-              checkboxes = document.querySelectorAll('input[type="checkbox"]'),
-              send = document.querySelector('.form-btn'),
-              reset = document.querySelector('.form-btn-reset');
-      let toggle1,
-          toggle2;
+(function() {
+    const inputs = document.querySelectorAll('.form-input'),
+		checkboxes = document.querySelectorAll('input[type="checkbox"]'),
+		send = document.querySelector('.form-btn'),
+		reset = document.querySelector('.form-btn-reset');
+		  
+		let toggle1,
+          	toggle2;
     
-      const allElements = [...Array.from(inputs), ...Array.from(checkboxes)];
+    const allElements = [...Array.from(inputs), ...Array.from(checkboxes)];
     
-      const checkForm2 = () => {
-          if(checkboxes[0].checked || checkboxes[1].checked) {
-              toggle2 = true;
-          } else {
-              toggle2 = false;
-          };
+    const checkForm2 = () => {
+        if(checkboxes[0].checked || checkboxes[1].checked) {
+            toggle2 = true;
+        } else {
+            toggle2 = false;
+        };
     
         for (let i = 0; i < inputs.length; i++) {
-          if(inputs[i].value !== '') {
+          	if(inputs[i].value !== '') {
               toggle2 = true;
-          } 
+          	} 
         };
     
         if(toggle2) {
-          reset.classList.add('form-btn-reset-active');
+          	reset.classList.add('form-btn-reset-active');
         } else {
-          reset.classList.remove('form-btn-reset-active');
+          	reset.classList.remove('form-btn-reset-active');
         };
       };
     
-      const checkForm = () => {
-          if(checkboxes[0].checked || checkboxes[1].checked) {
-              toggle1 = true;
-          } else {
-              toggle1 = false;
-          };
+    const checkForm = () => {
+		if(checkboxes[0].checked || checkboxes[1].checked) {
+			toggle1 = true;
+		} else {
+			toggle1 = false;
+		};
     
         for (let i = 0; i < inputs.length; i++) {
-          if(inputs[i].value !== '') {
-              toggle1 = true;
-          } else {
-              toggle1 = false;
-          };
+          	if(inputs[i].value !== '') {
+              	toggle1 = true;
+          	} else {
+              	toggle1 = false;
+          	};
         };
     
-          if(toggle1) {
-              send.classList.add('form-btn-send');
-          } else {
-              send.classList.remove('form-btn-send');
-          };
-      };
+          	if(toggle1) {
+              	send.classList.add('form-btn-send');
+          	} else {
+              	send.classList.remove('form-btn-send');
+          	};
+      	};
     
-      for (let i = 0; i < allElements.length; i++) {
-          allElements[i].addEventListener('change', checkForm);
-          allElements[i].addEventListener('change', checkForm2);
-      };
+      	for (let i = 0; i < allElements.length; i++) {
+			allElements[i].addEventListener('change', checkForm);
+			allElements[i].addEventListener('change', checkForm2);
+      	};
     
-      const validateFrom = evt => {
-          evt.preventDefault();
-          if (!toggle1) {
-              for (let i = 0; i < inputs.length; i++) {
-              if(inputs[i].value == '') {
-                  inputs[i].classList.add('form-input-invalid');
-              } else {
-                  inputs[i].classList.remove('form-input-invalid');
-              }
-          };
-          if(!checkboxes[0].checked && !checkboxes[1].checked) {
-              checkboxes.forEach( elem => {
-              console.log(elem)
-              elem.classList.add('checkbox-invalid')
+      	const validateFrom = evt => {
+          	evt.preventDefault();
+          	if (!toggle1) {
+				for (let i = 0; i < inputs.length; i++) {
+					if(inputs[i].value == '') {
+						inputs[i].classList.add('form-input-invalid');
+					} else {
+						inputs[i].classList.remove('form-input-invalid');
+					}
+        };
+          	if(!checkboxes[0].checked && !checkboxes[1].checked) {
+				checkboxes.forEach( elem => {
+				console.log(elem)
+				elem.classList.add('checkbox-invalid')
             }
             );
           } else {
@@ -83,8 +81,8 @@
             );
           };
         };
-      };
-      send.addEventListener('click', evt => validateFrom(evt));
+    };
+      	send.addEventListener('click', evt => validateFrom(evt));
   });
 
 
@@ -95,27 +93,27 @@
 
 
 var accordion = (function () {
-  return function (selector) {
-  var _mainElem = document.querySelector(selector),
+	return function (selector) {
+	var _mainElem = document.querySelector(selector),
     _items = _mainElem.querySelectorAll('.accordion__item');
 
-  for(var i = 0; i < _items.length; i++) {
+  	for(var i = 0; i < _items.length; i++) {
     _items[i].addEventListener('click', function () {
 
-      if (_mainElem.querySelector('.accordion__item--open') && _mainElem.querySelector('.accordion__item--open') != this) {
-        _mainElem.querySelector('.accordion__item--open .accordion-content').style.maxHeight = null;
-        _mainElem.querySelector('.accordion__item--open').classList.remove('accordion__item--open');
-      }	
+		if (_mainElem.querySelector('.accordion__item--open') && _mainElem.querySelector('.accordion__item--open') != this) {
+		_mainElem.querySelector('.accordion__item--open .accordion-content').style.maxHeight = null;
+		_mainElem.querySelector('.accordion__item--open').classList.remove('accordion__item--open');
+		}	
 
-      var itemContent = this.querySelector('.accordion-content');				
+      	var itemContent = this.querySelector('.accordion-content');				
       
-      if (itemContent.style.maxHeight) {
-        itemContent.style.maxHeight = null;
-      } else {
-        itemContent.style.maxHeight = itemContent.scrollHeight + "px";
-      }
+		if (itemContent.style.maxHeight) {
+			itemContent.style.maxHeight = null;
+		} else {
+			itemContent.style.maxHeight = itemContent.scrollHeight + "px";
+		}
 
-      this.classList.toggle('accordion__item--open');
+      	this.classList.toggle('accordion__item--open');
     });
   }
   }
