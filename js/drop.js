aabb7e8415f69e19cd8ec88c039daf88094d4afb
Oksana <oksana.kyrychuk1991@gmail.com>
2020-04-07 14:32:02 +0300

    Update

--- js/drop.js
+++ js/drop.js
@@ -1,29 +1,75 @@
+// Tab FAQ
+
 document.addEventListener("DOMContentLoaded", function() {
-    // === tabs
+	let tabButton = document.querySelectorAll(".tabs__switcher-button");
+	let tabSlide = document.querySelectorAll(".tabs__content");
+	let listItem = document.querySelectorAll(".list-item");
+
+//tabs switching
+
+	for(let i = 0; i < tabButton.length; i++) {
+		tabButton[i].onclick = () => {
+			tabButton.forEach((item, i) => {
+			item.classList.remove("active");
+		});
+
+		tabSlide.forEach((item, i) => {
+		item.classList.remove("visible");
+
+			for(let j = 1; j < item.children.length; j++) {
+				item.children[j].classList.remove("list-item-open");
+			}
+
+			item.children[0].classList.add("list-item-open");
+		});
+
+			tabButton[i].classList.add("active");
+			tabSlide[i].classList.add("visible");
+		};
+	}
+
+	//accordion
+
+	for(let i = 0; i < listItem.length; i++) {
+
+		listItem[i].onclick = () => {
+
+			if(!listItem[i].classList.contains("list-item-open")) {
+				listItem.forEach((item, i) => {
+				item.classList.remove("list-item-open");
+				});
+				listItem[i].classList.add("list-item-open");
+			} else {
+				listItem[i].classList.remove("list-item-open");
+			}
+		}
+	}
+
+	// === tabs
   
     const tabsBlock = document.querySelector(".tabs__block");
     const inner = document.querySelectorAll(".tabs__inner");
     const tabListItem = document.querySelectorAll(".tabs__list_item");
   
     Array.from(tabListItem, el =>
-      el.addEventListener("click", e => {
-        const dataTab = el.dataset.tab;
-        const tabInner = document.querySelector(
-          `.tabs__inner[data-tab='${dataTab}']`
+      	el.addEventListener("click", e => {
+			const dataTab = el.dataset.tab;
+			const tabInner = document.querySelector(
+          	`.tabs__inner[data-tab='${dataTab}']`
         );
   
         Array.from(inner, elInner => {
-          elInner.classList.remove("active");
+          	elInner.classList.remove("active");
         });
   
         tabInner.classList.add("active");
   
         Array.from(tabListItem, item => {
-          item.classList.remove("active");
+          	item.classList.remove("active");
         });
   
         el.classList.add("active");
-      })
+      	})
     );
   
     // === button
@@ -31,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.querySelector(".show_tabs");
   
     toggleButton.addEventListener("click", e => {
-      tabsBlock.classList.toggle("active");
+      	tabsBlock.classList.toggle("active");
     });
-  });
+});
   
